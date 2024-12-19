236f22a68883a9b5938e18d9c91ef9f31a029a4c
stella~ <choco@choco.rip>
2024-12-20 05:08:22 +0900

    Delete a.js
    Signed-off-by: stella~ <choco@choco.rip>

--- a.js
+++
@@ -1,7 +0,0 @@
-function makeBranchName() {
-    const date = new Date().toISOString().split('T')[0];
-    const random = Math.random().toString(36).substring(2, 15);
-    return `feature/${date}-${random}`;
-}
-
-console.log(makeBranchName())
