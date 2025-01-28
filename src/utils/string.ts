e27f2329159dfbca14fd1497f68f341cc3aaa1cc
stella~ <choco@choco.rip>
2025-01-29 08:12:45 +0900

    Simplify social platform name normalization

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,8 +1,3 @@
 export function normalizeSocial(social: string) {
-    // Something like this
-    // github -> GitHub
-    // twitter -> Twitter
-    // linkedin -> LinkedIn
-    // etc.
     return social.charAt(0).toUpperCase() + social.slice(1);
 }
